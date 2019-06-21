0c8bc73a1877be6792201c527842958740917001
alexnbl@gmail.com <alexnbl@gmail.com>
2019-06-20 22:47:26 -0700

    added directories and bash script

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat templates/top.html docs/aboutmiddle.html templates/bottom.html > docs/about.html
+cat templates/top.html docs/gallerymiddle.html templates/bottom.html > docs/gallery.html
+cat templates/top.html docs/singlemiddle.html templates/bottom.html > docs/single.html
